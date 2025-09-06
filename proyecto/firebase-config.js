83c654ff3413955aaca0e86ec2a6a31b0b19c351
Brunella Figallo <162316949+BruneBerryy@users.noreply.github.com>
2025-09-05 21:18:00 -0300

    Add files via upload

---
+++ proyecto/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCVNU-cb9DQRk395LE2tjhI7FJKBjkzvXg",
+    authDomain: "empresa-tecnologia.firebaseapp.com",
+    projectId: "empresa-tecnologia",
+    storageBucket: "empresa-tecnologia.firebasestorage.app",
+    messagingSenderId: "317305921158",
+    appId: "1:317305921158:web:b863bf634cfe6adcaffb07",
+    measurementId: "G-3P63NE5HQF"
+  };
